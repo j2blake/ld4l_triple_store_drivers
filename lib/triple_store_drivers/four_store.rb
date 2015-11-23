57a9252ca01d3a0220be424af633fde485e86c0c
Jim Blake <jeb228@cornell.edu>
2015-11-23 13:55:06 -0500

    Specify a data directory for 4store.

--- lib/triple_store_drivers/four_store.rb
+++ lib/triple_store_drivers/four_store.rb
@@ -11,11 +11,11 @@ For size, go to http://localhost:8000/status/size/
 
 --------------------------------------------------------------------------------
 =end
+
 module TripleStoreDrivers
   class FourStore
     include TripleStoreDrivers::BaseDriver
     include TripleStoreDrivers::HttpHandler
-    
     class << self
       def set_instance(instance, settings)
         @instance = instance
@@ -46,14 +46,21 @@ module TripleStoreDrivers
     # All of the parameters have reasonable defaults.
     #
     DEFAULT_PARAMS = {
+      :base_data_dir => '/var/lib/4store',
+      :data_dir => '/ACTUAL/DATA/DIRECTORY/NOT/SPECIFIED',
       :seconds_to_startup => 30,
       :db_name => 'mydb'
     }
 
     def initialize(params)
       @settings = DEFAULT_PARAMS.merge(params)
-      self.class.set_instance(self, @settings)
+
+      @data_dir = @settings[:data_dir]
+      raise IllegalStateError.new("Data directory doesn't exist: #{@data_dir}") unless Dir.exists?(@data_dir)
+
+      @base_data_dir = @settings[:base_data_dir]
       @db_name = @settings[:db_name]
+      self.class.set_instance(self, @settings)
     end
 
     def running?()
@@ -68,6 +75,24 @@ module TripleStoreDrivers
         end
       end
       false
+    end
+
+    def create
+      link_to = File.expand_path(@db_name, @data_dir)
+      link = File.expand_path(@db_name, @base_data_dir)
+      
+      raise IllegalStateError.new("Data directory already exists: #{link_to}") if Dir.exists?(link_to)
+      raise IllegalStateError.new("Link to data directory already exists: #{link}") if Dir.exists?(link)
+
+      puts `4s-backend-setup #{@db_name}`
+      raise "4s-backend-setup failed: exit status = #{$?.exitstatus}" unless $?.exitstatus == 0
+
+      `mv #{link} #{link_to}`
+      raise "Failed to move the data directory: exit status = #{$?.exitstatus}" unless $?.exitstatus == 0
+      bogus "MADE DIRECTORY: #{link_to}"
+
+      puts `ln -s #{link_to} #{link}`
+      raise "Failed to create the link: exit status = #{$?.exitstatus}" unless $?.exitstatus == 0
     end
 
     def open
@@ -86,12 +111,12 @@ module TripleStoreDrivers
 
     def close
       puts "Closing #{self}."
-      
+
       unless `pgrep 4s-httpd`.empty?
         puts `pkill 4s-httpd`
         raise "Failed to stop 4store frontend: exit status = #{$?.exitstatus}" unless $?.exitstatus == 0
       end
-  
+
       unless `pgrep 4s-backend`.empty?
         puts `pkill 4s-backend`
         raise "Failed to stop 4store backend: exit status = #{$?.exitstatus}" unless $?.exitstatus == 0
@@ -141,10 +166,10 @@ module TripleStoreDrivers
     def clear()
       raise IllegalStateError.new("Clear not permitted on #{self}") unless clear_permitted?
       raise IllegalStateError.new("#{self} is running") if running?
-      
+
       puts `4s-backend-destroy #{@db_name}`
       raise "Failed to destroy 4store repository: exit status = #{$?.exitstatus}" unless $?.exitstatus == 0
-  
+
       puts `4s-backend-setup #{@db_name}`
       raise "Failed to create 4store repository: exit status = #{$?.exitstatus}" unless $?.exitstatus == 0
     end
