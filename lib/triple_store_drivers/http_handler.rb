e8209def06c9043755d0bedaf4296036c93981fb
Jim Blake <jeb228@cornell.edu>
2015-11-23 14:29:41 -0500

    Add an http_get method to HttpHandler. Clumsy but functional.

--- lib/triple_store_drivers/http_handler.rb
+++ lib/triple_store_drivers/http_handler.rb
@@ -1,5 +1,52 @@
 module TripleStoreDrivers
   module HttpHandler
+    def http_get(url, block, params, headers={})
+      uri = URI.parse(url)
+
+      unless params.empty?
+        joined_params = params.to_a.map {|p| p[0] + '=' + p[1]}.join('&')
+        if uri.query.empty?
+          uri.query = joined_params
+        else
+          uri.query = uri.query + '&' + joined_params
+        end
+      end
+
+      Net::HTTP.start(uri.host, uri.port) do |http|
+        http.read_timeout = 180
+
+        request = Net::HTTP::Get.new(uri.request_uri)
+        headers.each_pair do |k, v|
+          request[k] = v
+        end
+
+        begin
+          http.request(request) do |response|
+            if response.code == "301"
+              follow_redirect
+              redirect = Net::HTTP::Post.new(URI.parse(response.header['location']).request_uri)
+              headers.each_pair do |k, v|
+                redirect[k] = v
+              end
+              response = http.request(redirect)
+            end
+
+            if response.code == "200"
+              block.call(response) if block
+            else
+              if block_given?
+                yield response
+              else
+                response.value
+              end
+            end
+          end
+        rescue IOError => e
+          raise e.exception(e.message << "\nProblem request: \n#{inspect_request(request, url)}")
+        end
+      end
+    end
+
     def http_post(url, block, params, headers={})
       uri = URI.parse(url)
 
